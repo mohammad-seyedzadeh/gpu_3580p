55c332e45bf3190de2a9000efd54771cc8b73368
seyedzadeh <ses174@pitt.edu>
2017-12-05 19:58:07 -0500

    added parser_1

---
+++ parser.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cassert>
+#include<map>
+#include<vector>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <ctime>
+#include <cstddef>
+using namespace std;
+int main ()
+{
+
+
+int i=5;
+
+cout<<"\n"<<(i/8)<<"\n";
+
+	
+
+  return 0;
+}
